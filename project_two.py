bc1a2c0c97993db9b24d8daf8fe85bb94d15aaa8
Diana B <114450141+diana-bergstrom@users.noreply.github.com>
2023-04-21 16:17:49 -0700

    Update and rename project_two to project_two.py

--- project_two.py
+++ project_two.py
@@ -40,3 +40,23 @@ def calculate_demographic_data(print_data=True):
     total_bachelors = df[df['education'] == 'Bachelors']['education'].count()
     total_education = df['education'].count()
     percentage_bachelors = ((total_bachelors / total_education) * 100).round(1)
+    
+    # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
+    # What percentage of people without advanced education make more than 50K?
+
+    # with and without `Bachelors`, `Masters`, or `Doctorate`
+    higher_education = df[df['education'].isin(['Bachelors', 'Masters', 'Doctorate'])]
+    lower_education = df[~df['education'].isin(['Bachelors', 'Masters', 'Doctorate'])]
+
+    # percentage with salary >50K
+    higher_ed_over = len(higher_education[higher_education['salary'] == '>50K'])
+    total_higher_ed = len(higher_education)
+    percent_higher_ed_over = (higher_ed_over / total_higher_ed) * 100
+    higher_education_rich = round(percent_higher_ed_over, 1)
+
+    lower_ed_over = len(lower_education[lower_education['salary'] == '>50K'])
+    total_lower_ed = len(lower_education)
+    percent_lower_ed_over = (lower_ed_over / total_lower_ed) * 100
+
+    lower_education_rich = round(percent_lower_ed_over, 1)
+
