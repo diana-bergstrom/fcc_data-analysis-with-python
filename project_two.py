b76be5421c5ef70eaa0e5c53eb2df6df0ac5867b
Diana B <114450141+diana-bergstrom@users.noreply.github.com>
2023-04-21 20:12:26 -0700

    Update project_two.py

--- project_two.py
+++ project_two.py
@@ -37,6 +37,7 @@ def calculate_demographic_data(print_data=True):
     average_age_men = df[df['sex'] == 'male']['age'].mean().round(1)
 
     # What is the percentage of people who have a Bachelor's degree?
+    # Count total bachelors and total people with any level of education
     total_bachelors = df[df['education'] == 'Bachelors']['education'].count()
     total_education = df['education'].count()
     percentage_bachelors = ((total_bachelors / total_education) * 100).round(1)
@@ -59,4 +60,57 @@ def calculate_demographic_data(print_data=True):
     percent_lower_ed_over = (lower_ed_over / total_lower_ed) * 100
 
     lower_education_rich = round(percent_lower_ed_over, 1)
+    
+    # What is the minimum number of hours a person works per week (hours-per-week feature)?
+    min_work_hours = df['hours-per-week'].min()
+
+    # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
+    num_min_workers = df[df['hours-per-week'] == min_work_hours]
+    rich_min_workers = len(num_min_workers[(num_min_workers['salary'] == '>50K')])
+
+    rich_percentage = round((rich_min_workers / len(num_min_workers) * 100), 1)
+
+    # What country has the highest percentage of people that earn >50K?
+    country_count = df['native-country'].value_counts()
+    rich_country_count = df[df['salary'] == '>50K']['native-country'].value_counts()
+    
+    highest_earning_country = (rich_country_count / country_count * 100).idxmax()
+    highest_earning_country_percentage = round((rich_country_count / country_count * 100), 1).max()
+  
+    # Identify the most popular occupation for those who earn >50K in India.
+  
+    rich_workers_india = df[(df['native-country'] == 'India') & (df['salary'] == '>50K')]
+    occupation_count = rich_workers_india['occupation'].value_counts()
+    
+    # Need to use idxmax to find the idex of the occupation with the most workers that fit the criteria
+    # Using just max only finds the maximumm number of occupation count for workers in India that make >50K
+    top_IN_occupation = occupation_count.idxmax()
+
+    # DO NOT MODIFY BELOW THIS LINE
+
+    if print_data:
+        print("Number of each race:\n", race_count) 
+        print("Average age of men:", average_age_men)
+        print(f"Percentage with Bachelors degrees: {percentage_bachelors}%")
+        print(f"Percentage with higher education that earn >50K: {higher_education_rich}%")
+        print(f"Percentage without higher education that earn >50K: {lower_education_rich}%")
+        print(f"Min work time: {min_work_hours} hours/week")
+        print(f"Percentage of rich among those who work fewest hours: {rich_percentage}%")
+        print("Country with highest percentage of rich:", highest_earning_country)
+        print(f"Highest percentage of rich people in country: {highest_earning_country_percentage}%")
+        print("Top occupations in India:", top_IN_occupation)
+
+    return {
+        'race_count': race_count,
+        'average_age_men': average_age_men,
+        'percentage_bachelors': percentage_bachelors,
+        'higher_education_rich': higher_education_rich,
+        'lower_education_rich': lower_education_rich,
+        'min_work_hours': min_work_hours,
+        'rich_percentage': rich_percentage,
+        'highest_earning_country': highest_earning_country,
+        'highest_earning_country_percentage':
+        highest_earning_country_percentage,
+        'top_IN_occupation': top_IN_occupation
+    }
 
